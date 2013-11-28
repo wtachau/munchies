f39917d040284166059e281bf3ab2e070a5e97f3
Will Tachau <wtachau@gmail.com>
2013-11-28 15:59:46 -0800

    started making view

--- claremont_munchies_site/claremont_munchies/views.py
+++ claremont_munchies_site/claremont_munchies/views.py
@@ -1,1 +1,14 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.shortcuts import render
+from django.template.loader import *
+
+import datetime
+
+
+def hello(request):
+    return HttpResponse("<h1>Hello Page</h1>Hello world")
+
+def order_form(request):
+    t = get_template('order_form.html')
+    html = t.render(Context())
+    return HttpResponse(html) 
