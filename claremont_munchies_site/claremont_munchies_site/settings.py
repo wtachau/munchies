f0cf6105d723cf66185f7d547ae95cc658353883
James Reinke <james@Jamess-MacBook-Pro-2.local>
2013-11-28 16:16:09 -0800

    setting.py changed

--- claremont_munchies_site/claremont_munchies_site/settings.py
+++ claremont_munchies_site/claremont_munchies_site/settings.py
@@ -123,7 +123,7 @@ INSTALLED_APPS = (
     'django.contrib.sites',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'orders',
+    'claremont_munchies',
     # Uncomment the next line to enable the admin:
     # 'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
