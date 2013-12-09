10ca3544baad0e745dd8cf7288802129acfc37ba
Will Tachau <wtachau@gmail.com>
2013-12-09 01:21:38 -0800

    adding changes to db

--- claremont_munchies_site/claremont_munchies/models.py
+++ claremont_munchies_site/claremont_munchies/models.py
@@ -11,12 +11,21 @@ class user(models.Model):
     claremont_id_number = models.CharField(max_length=20, null=True)
     email = models.CharField(max_length=255)
     stripe_id = models.CharField(null=True, max_length=255)
+    registered_timestamp = models.DateTimeField(auto_now_add=True)
     
 class orders(models.Model):
     credit_card_token = models.CharField(max_length=255)
     order = models.CharField(max_length=500)
     date = models.DateField(auto_now = True, auto_now_add=True)
     total_amount = models.IntegerField()
+
+class deals(models.Model):
+    deal_type = models.CharField(max_length=11)
+    deal_value = models.IntegerField()
+
+class dealInstances(models.Model):
+    user_id = models.IntegerField()
+    deal_id = models.IntegerField()
     
     
     
