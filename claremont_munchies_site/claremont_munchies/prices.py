d367a8b25dd10f31b5ae03135b2cb8ef152718c0
Will Tachau <wtachau@gmail.com>
2014-01-22 16:37:00 -0800

    cleared orders on login, updated prices

--- claremont_munchies_site/claremont_munchies/prices.py
+++ claremont_munchies_site/claremont_munchies/prices.py
@@ -7,13 +7,13 @@ def get_prices():
     context['cheeseburger_2'] = 6.79
     context['cheeseburger_3'] = 7.99
     context['cheeseburger_4'] = 8.99
-    context['grilled_cheese'] = 3.49
+    context['grilled_cheese'] = 4.99
     context['hamburger_1'] = 4.99
     context['hamburger_2'] = 6.29
     context['hamburger_3'] = 7.49
     context['hamburger_4'] = 8.49
-    context['fries'] = 2.99
+    context['fries'] = 2.49
     context['shake'] = 4.99
     context['drink'] = 0.99
-    context['animal_fries'] = 5.99
+    context['animal_fries'] = 4.99
     return context
