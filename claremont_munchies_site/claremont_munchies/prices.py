aeb27b3f091ba60e9b6a66b03f0184cb145d57e3
Will Tachau <wtachau@gmail.com>
2014-01-29 18:03:49 -0800

    changed prices, fixed typo

--- claremont_munchies_site/claremont_munchies/prices.py
+++ claremont_munchies_site/claremont_munchies/prices.py
@@ -3,17 +3,17 @@ from django.template.loader import *
 context = Context()
 
 def get_prices():
-    context['cheeseburger_1'] = 5.49
-    context['cheeseburger_2'] = 6.79
-    context['cheeseburger_3'] = 7.99
-    context['cheeseburger_4'] = 8.99
-    context['grilled_cheese'] = 4.99
-    context['hamburger_1'] = 4.99
-    context['hamburger_2'] = 6.29
-    context['hamburger_3'] = 7.49
-    context['hamburger_4'] = 8.49
-    context['fries'] = 2.49
-    context['shake'] = 4.99
+    context['cheeseburger_1'] = 3.79
+    context['cheeseburger_2'] = 4.79
+    context['cheeseburger_3'] = 5.99
+    context['cheeseburger_4'] = 7.29
+    context['grilled_cheese'] = 3.49
+    context['hamburger_1'] = 3.49
+    context['hamburger_2'] = 4.59
+    context['hamburger_3'] = 5.99
+    context['hamburger_4'] = 7.29
+    context['fries'] = 2.29
+    context['shake'] = 4.49
     context['drink'] = 0.99
-    context['animal_fries'] = 4.99
+    context['animal_fries'] = 4.79
     return context
