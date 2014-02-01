0b15ecf8857b2807a828f07018d643ce5fc4d22e
Will Tachau <wtachau@gmail.com>
2014-01-31 16:47:28 -0800

    fixed bugs

--- claremont_munchies_site/claremont_munchies/prices.py
+++ claremont_munchies_site/claremont_munchies/prices.py
@@ -4,13 +4,13 @@ context = Context()
 
 def get_prices():
     context['cheeseburger_1'] = 3.99
-    context['cheeseburger_2'] = 4.79
+    context['cheeseburger_2'] = 5.29
     context['cheeseburger_3'] = 6.49
     context['cheeseburger_4'] = 7.99
     context['grilled_cheese'] = 3.49
     context['hamburger_1'] = 3.49
-    context['hamburger_2'] = 4.29
-    context['hamburger_3'] = 5.99
+    context['hamburger_2'] = 4.89
+    context['hamburger_3'] = 6.49
     context['hamburger_4'] = 7.49
     context['fries'] = 2.39
     context['shake'] = 4.49
