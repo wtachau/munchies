3bae18a2f9ee2cddeab648108881da7665084905
Will Tachau <wtachau@gmail.com>
2014-02-12 23:38:39 -0800

    new mexican menu

--- claremont_munchies_site/claremont_munchies/prices.py
+++ claremont_munchies_site/claremont_munchies/prices.py
@@ -3,6 +3,7 @@ from django.template.loader import *
 context = Context()
 
 def get_prices():
+    # burgers
     context['cheeseburger_1'] = 3.99
     context['cheeseburger_2'] = 5.29
     context['cheeseburger_3'] = 6.49
@@ -16,4 +17,17 @@ def get_prices():
     context['shake'] = 4.49
     context['drink'] = 0.99
     context['animal_fries'] = 4.99
+
+    #mexican
+    context['burrito_carne'] = 6.49
+    context['burrito_beans'] = 4.99
+    context['burrito_beef'] = 5.99
+    context['burrito_chicken'] = 5.99
+    context['taco_beef'] = 3.29
+    context['taco_chicken'] = 3.29
+    context['taco_carne'] = 3.79
+    context['taco_tacquitos'] = 4.99
+    context['quesadilla_cheese'] = 4.99
+    context['quesadilla_chicken'] = 6.29
+    context['quesadilla_beef'] = 6.29
     return context
